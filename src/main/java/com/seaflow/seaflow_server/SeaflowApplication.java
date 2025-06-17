fe5a1bdabaa4703d34341336c18ae98594720239
Passbob <gudjtr097@gmail.com>
2025-06-17 09:27:43 +0900

    Initial commit

---
+++ src/main/java/com/seaflow/seaflow_server/SeaflowApplication.java
@@ -0,0 +1,13 @@
+package com.seaflow.seaflow_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeaflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seaflow/seaflow_server/SeaflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seaflow.seaflow_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
